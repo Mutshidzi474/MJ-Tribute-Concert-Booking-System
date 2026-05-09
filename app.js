bfe190ec8c5c04918e3cd91ce724d53b9993f0a7
ThaskyTech <LTMolise@outlook.com>
2026-05-09 20:14:35 +0200

    1st Middlewware update

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
+const express=require('express');
+const session=require('express-session');
+const MongoStore=require('connect-mongo');
+const mongoose=require('mongoose');
+const dotenv=require('dotenv');
+const path=require('path');
+
+dotenv.config();// Loading of the .env file intoprocess.env
+
+const app=express();
+
+//Parsing and Middlewares
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname+'public')));
+app.use('view engine'+'ejs');
+app.use('views'+path.join(__dirname+'views'));
+app.use(session({
+    secret:process.env.SESSION_SECRET,
+    resave:false,
+    saveUninitialized:false,
+    store:MongoStore.createKrupteinAdapter({mongoUrl:process.env.MONGO_URI}),
+    cookie:{maxAge:1000*60*60*24}
+}));
+
+//Database Connection
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>console,log('MongoDB connected Succesfully'))
+    .catch((error)=>console.log(`MongoDB connection Unsuccesful: ${error}`));
+
+
+//*************************F I L L  I N  R O U T E S  C O D E  H E R E ********************************
+//let me know if you need assistance here or you can ask Mutshidzi
+
+
+//Server
+const port=process.env.PORT || 3000;
+app.listen(port,()=>console.log(`Server running on port ${port}`));
+
+
 
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,1 +1,17 @@
+const isLoggedIn=(req,res,next)=>{
+    if(req.session && req.session.userId){
+        return next();
+    }
+    res.redirect('/auth/login');
+};
+
+const isAdmin=(req,res,next)=>{
+    if(req.session && req.session.role==='admin'){
+        return next();
+    }
+    res.status(403).render('error',{message:'Access denied. Admins only!!'});
+};
+
+module.exports={isLoggedIn,isAdmin};
+
 
