0e67a6b7e3d40c696ae0306f2da6f5ee2470204b
cheezy <mutshidzi2005@gmail.com>
2026-05-12 13:00:22 +0200

    Updated authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,3 +1,5 @@
+const { Router } = require("express");
+
 const isLoggedIn=(req,res,next)=>{
     if(req.session && req.session.userId){
         return next();
@@ -13,5 +15,29 @@ const isAdmin=(req,res,next)=>{
 };
 
 module.exports={isLoggedIn,isAdmin};
+
+//Validation middleware for user registration
+const validateUserRegistration=[
+    //Validation for username
+    body("username")
+    .notEmpty()
+    .withMessage("Username is required")
+    .isLength({min:3})
+    .withMessage("Username must be at least 3 characters long"),
+
+    //Validation for email
+    body("email")
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Please enter a valid email address"),
+
+    //Validation for password
+    body("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({min:6})
+    .withMessage("Password must be at least 6 characters long")
+];
 
 
