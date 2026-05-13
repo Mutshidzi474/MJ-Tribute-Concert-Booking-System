13c5d022da8547de1a93f8e7ef9d16a2a439f99c
cheezy <mutshidzi2005@gmail.com>
2026-05-13 15:25:22 +0200

    Update Booking.js

--- models/Booking.js
+++ models/Booking.js
@@ -1,1 +1,44 @@
+const mongoose = require('mongoose');
 
+const bookingSchema = new mongoose.Schema({
+
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+
+    userName: {
+        type: String,
+        required: true
+    },
+
+    eventId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Event',
+        required: true
+    },
+
+    eventName: {
+        type: String,
+        required: true
+    },
+
+    tickets: {
+        type: Number,
+        required: true
+    },
+
+    status: {
+        type: String,
+        default: 'Confirmed'
+    },
+
+    bookingDate: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+module.exports = mongoose.model('Booking', bookingSchema);
