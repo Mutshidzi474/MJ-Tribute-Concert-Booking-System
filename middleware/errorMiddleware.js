071c579189fe7b88049481f1343ecef2bdcc311e
cheezy <mutshidzi2005@gmail.com>
2026-05-13 05:08:14 +0200

    Update errorMiddleware.js

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -1,13 +1,17 @@
-const notFound=(req,res,next)=>{
-    res.status(404).render('error',{message:`Page not found - ${req.originalUrl}`});
+const notFound = (req, res, next) => {
+    res.status(404).json({
+        success: false,
+        message: `Page not found - ${req.originalUrl}`
+    });
 };
 
-const errorHandler=(error,req,res,next)=>{
+const errorHandler = (error, req, res, next) => {
     console.error(error.stack);
-    res.status(error.status || 500).render('error',{message:error.message || 'Something went wrong on our end'});
+
+    res.status(error.status || 500).json({
+        success: false,
+        message: error.message || "Something went wrong on our end"
+    });
 };
 
-module.exports={notFound,errorHandler};
-
-const {body,validationResult}=require('express-validator');
-
+module.exports = { notFound, errorHandler };
