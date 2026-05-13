767fe1035413bae2665276ffaf192d442e655630
cheezy <mutshidzi2005@gmail.com>
2026-05-13 15:16:03 +0200

    Update hash.js

--- config/hash.js
+++ config/hash.js
@@ -1,8 +1,8 @@
-const bcrypt=require('bcryptjs');
+const bcrypt=require('bcryptjs'); // importing bcryptjs for password hashing and comparison
 const hashPassword=async(password)=>{
     const salt=await bcrypt.genSalt(10);
     return await bcrypt.hash(password,salt);
 };
-const comparePassword=async(plainPassword,hashedPassword)=>{return await bcrypt.compare(plainPassword,hashPassword)};
+const comparePassword=async(plainPassword,hashedPassword)=>{return await bcrypt.compare(plainPassword,hashedPassword)};
 
 module.exports={hashPassword,comparePassword};
