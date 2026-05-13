ec73c1a42444f4d696f7b99795a9886cfd51e701
cheezy <mutshidzi2005@gmail.com>
2026-05-13 15:25:41 +0200

    Update Event.js

--- models/Event.js
+++ models/Event.js
@@ -1,1 +1,16 @@
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+    title: String,
+    date: Date,
+    category: String,
+    location: String,
+    description: String,
+    capacity: Number,
+    price: { type: Number, min: 400 },
+    ticketAvailable: Number
+
+});
+
+module.exports = mongoose.model('Event', eventSchema);
 
