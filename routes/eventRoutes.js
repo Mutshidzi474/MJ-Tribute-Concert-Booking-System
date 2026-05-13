ddef1d8a53a0fd46b65ef3e8912a64d352c3f882
cheezy <mutshidzi2005@gmail.com>
2026-05-13 22:14:28 +0200

    Update eventRoutes.js

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const eventController = require('../controllers/eventController');
 const { isAdmin } = require('../middleware/authMiddleware');
 
+router.get('/events', eventController.getEvents);
 router.post('/events/create', isAdmin, eventController.createEvent);
 router.get('/events/edit/:id', isAdmin, eventController.getEditEvent);
 router.post('/events/:id/update', isAdmin, eventController.updateEvent);
 router.post('/events/:id/delete', isAdmin, eventController.deleteEvent);
 
 module.exports = router;
-
